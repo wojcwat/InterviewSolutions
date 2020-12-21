026b829c0769194b16399332893b3c3fce3f5392
wojcwat <aeslin@wp.pl>
2020-12-21 18:53:31 +0100

    Task1 code refactoring

--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -6,40 +6,33 @@ namespace Task1
 {
     class ListOperations
     {
-        private const int TabSize = 10;
-        private const int MinRandomValue = 0;
-        private const int MaxRandomValue = 101;
-        private const int ShadowNeighbourValue = 50;
+        private const int randomNumbersCount = 200;
+        private const int minRandomValue = 0;
+        private const int maxRandomValue = 101;
+        private const int shadowNeighbourValue = 50;
         private int[] RandomNumbers;
-        public ListOperations()
+
+        public int[] GenerateRandomNumbers()
         {
             Random rng = new Random();
-            RandomNumbers = new int[TabSize];
+            RandomNumbers = new int[randomNumbersCount];
 
-            for (int i = 0; i < TabSize; ++i)
+            for (int i = 0; i < randomNumbersCount; ++i)
             {
-                RandomNumbers[i] = rng.Next(MinRandomValue, MaxRandomValue);
+                RandomNumbers[i] = rng.Next(minRandomValue, maxRandomValue);
             }
 
-            foreach (var i in RandomNumbers)
-            {
-                Console.WriteLine(i);
-            }
-            Console.WriteLine("=============");
+            return RandomNumbers;
         }
 
         public List<int> GetEveryThirdReverse(int[] numbersTab)
         {
             List<int> numbersList = new List<int>(numbersTab);
+
+            //Reverse list
             numbersList.Reverse();
-
-            //var everyFourth = list.Where((x,i) => i % 4 == 0);
-            var everyThird = numbersList.Where((x, i) => i % 3 == 0).ToList();
-
-            foreach (var i in everyThird)
-            {
-                Console.WriteLine(i);
-            }
+            //Get every third element
+            var everyThird = numbersList.Where((val, index) => index % 3 == 0).ToList();
 
             return everyThird;
         }
@@ -48,20 +41,33 @@ namespace Task1
         {
             List<int> result = new List<int>();
 
-            for (int i = 0; i < TabSize; ++i)
+            //first element
+            result.Add((shadowNeighbourValue + numbersTab[0] + numbersTab[1]) / 3);
+
+            //every element but last (and first)
+            for(int i=1; i< randomNumbersCount -2;++i)
             {
-                if (i == 0)
-                    result.Add((ShadowNeighbourValue + numbersTab[i] + numbersTab[i + 1]) / 3);
-                else if (i == TabSize - 1)
-                    result.Add((numbersTab[i - 1] + numbersTab[i] + ShadowNeighbourValue) / 3);
-                else
-                    result.Add((numbersTab[i - 1] + numbersTab[i] + numbersTab[i + 1]) / 3);
+                result.Add((numbersTab[i - 1] + numbersTab[i] + numbersTab[i + 1]) / 3);
             }
 
-            foreach(var i in result)
-            {
-                Console.WriteLine(i);
-            }
+            //last element
+            result.Add((numbersTab[randomNumbersCount - 2] + numbersTab[randomNumbersCount-1] + shadowNeighbourValue) / 3);
+
+
+            //Do the same within one loop
+
+            //for (int i = 0; i < randomNumbersCount; ++i)
+            //{
+            //    //For first and last elements add shadow neighbour
+            //    if (i == 0)
+            //        result.Add((shadowNeighbourValue + numbersTab[i] + numbersTab[i + 1]) / 3);
+            //    else if (i == randomNumbersCount - 1)
+            //        result.Add((numbersTab[i - 1] + numbersTab[i] + shadowNeighbourValue) / 3);
+
+            //    //count average for remaining elements
+            //    else
+            //        result.Add((numbersTab[i - 1] + numbersTab[i] + numbersTab[i + 1]) / 3);
+            //}
 
             return result;
         }
@@ -69,14 +75,10 @@ namespace Task1
         public List<int> GetNumbersGreaterThan90 (int[] numbersTab)
         {
             List<int> result = new List<int>(numbersTab);
+            //get numbers greater than 90 
             var greaterThan90 = from n in result
                                 where n > 90
                                 select n;
-
-            foreach(var i in greaterThan90.ToList())
-            {
-                Console.WriteLine(i);
-            }
 
             return greaterThan90.ToList();
         }
@@ -88,14 +90,17 @@ namespace Task1
 
         public List<Tuple<int, int>> GetParisOfProductGreaterThan9000 (int[] numbersTab)
         {
-            //List<int> result = new List<int>(numbersTab);
-            var pairs = new List<Tuple<int, int>>();
+            var pairs = new List<Tuple<int, int>>();  
+            
+            //tempTuple to avoid creating tuple twice in for loop
             Tuple<int, int> tempTuple;
+
+            //TupleComparer used for comparing equality of two tuples
             TupleComparer distinctTuples = new TupleComparer();
 
-            for (int i = 0; i<TabSize;++i)
+            for (int i = 0; i<randomNumbersCount;++i)
             {
-                for(int j = 0; j<TabSize; ++j)
+                for(int j = 0; j<randomNumbersCount; ++j)
                 {
                     if ((numbersTab[i] * numbersTab[j]) > 9000)
                     {
@@ -105,16 +110,10 @@ namespace Task1
                     }
                 }
             }
-
-            foreach(var i in pairs)
-            {
-                Console.WriteLine(i);
-            }
-
             return pairs;
-
         }
 
+        //TupleComparer - tuples (1,2) and (2,1) are equal
         public class TupleComparer : IEqualityComparer<Tuple<int, int>>
         {
             public bool Equals(Tuple<int, int> x, Tuple<int, int> y)
@@ -125,9 +124,7 @@ namespace Task1
 
             public int GetHashCode(Tuple<int, int> obj)
             {
-                return string.Concat(new int[] { obj.Item1, obj.Item2 }.OrderBy(x => x)).GetHashCode();
-                //or
-                //return (string.Compare(obj.Item1, obj.Item2) < 0 ? obj.Item1 + obj.Item2 : obj.Item2 + obj.Item1).GetHashCode(); 
+                return obj.Item1.GetHashCode() ^ obj.Item2.GetHashCode();
             }
         }
 
@@ -137,11 +134,48 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            ListOperations ListTask = new ListOperations();
-            //ListTask.GetEveryThirdReverse(ListTask.GetRandomNumbers());
-            //ListTask.GetNeighboursMeanValue(ListTask.GetRandomNumbers());
-            //ListTask.GetNumbersGreaterThan90(ListTask.GetRandomNumbers());
-            ListTask.GetParisOfProductGreaterThan9000(ListTask.GetRandomNumbers());
+            ListOperations listOperations = new ListOperations();
+
+            //Task1
+            var generatedNumbers = listOperations.GenerateRandomNumbers();
+
+            var result1 = listOperations.GetEveryThirdReverse(generatedNumbers);
+            var result2 = listOperations.GetNeighboursMeanValue(generatedNumbers);
+            var result3 = listOperations.GetNumbersGreaterThan90(generatedNumbers);
+            var result4 = listOperations.GetParisOfProductGreaterThan9000(generatedNumbers);
+
+            Console.WriteLine("Generated list:\n\n(index) = value");
+            //Display generated list 
+            for(int i = 0; i < generatedNumbers.Length; ++i)
+            {
+                Console.WriteLine("({0}) = {1}", i, generatedNumbers[i]);
+            }
+            //Task2
+            Console.WriteLine("\nEvery third element starting from the end of the list:");
+            foreach(var i in result1)
+            {
+                Console.Write(i + " ");
+            }
+            //Task3
+            Console.WriteLine("\n\nEvery element is a mean of current value and its neighbours:");
+            foreach(var i in result2)
+            {
+                Console.Write(i + " ");
+            }
+            //Task4
+            Console.WriteLine("\n\nElements greater than 90:");
+            foreach(var i in result3)
+            {
+                Console.Write(i + " ");
+            }
+            //Task5
+            Console.WriteLine("\n\nPairs of indexes with product greater than 9000:");
+            foreach(var i in result4)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
         }
     }
 }
