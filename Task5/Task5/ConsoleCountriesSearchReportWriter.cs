2cd0a413144ccc1b39b2ff55f34feb35ff420c82
wojcwat <aeslin@wp.pl>
2020-12-21 21:28:47 +0100

    Improved LatLng display formula

--- Task5/Task5/ConsoleCountriesSearchReportWriter.cs
+++ Task5/Task5/ConsoleCountriesSearchReportWriter.cs
@@ -14,16 +14,9 @@ namespace Task5
                 Console.WriteLine($"\t-stolica: {country.Capital}");
                 Console.WriteLine($"\t\t- ludność: {country.Population / 1e6} milionów");
                 Console.WriteLine($"\t\t- powierzchnia: {country.Area} km^2");
-
                 //To prevent bug when searching 'uni' -> United States Minor Outlying Islands have empty LatLng property
-                if (country.LatLng.Length == 0)
-                {
-                    double?[] fixedArray = { null, null };
-                    country.LatLng = fixedArray;
-                }
-
-                Console.WriteLine($"\t\t- szerokość geograficzna: {(country.LatLng[0] > 0 ? country.LatLng[0].ToString() + $"{Convert.ToChar(176)}(N)" : (country.LatLng[0] == null ? null : country.LatLng[0].ToString() + $"{Convert.ToChar(176)}(S)"))}");
-                Console.WriteLine($"\t\t- długość geograficzna:  {(country.LatLng[1] > 0 ? country.LatLng[1].ToString() + $"{Convert.ToChar(176)}(E)" : (country.LatLng[0] == null ? null : country.LatLng[0].ToString() + $"{Convert.ToChar(176)}(W)"))}");
+                Console.WriteLine($"\t\t- szerokość geograficzna: {(country.LatLng.Length == 0 ? null : (country.LatLng[0] > 0 ? country.LatLng[0].ToString() + $"{Convert.ToChar(176)}(N)" : (country.LatLng[0] == null ? null : country.LatLng[0].ToString() + $"{Convert.ToChar(176)}(S)")))}");
+                Console.WriteLine($"\t\t- długość geograficzna:  {(country.LatLng.Length == 0 ? null : (country.LatLng[1] > 0 ? country.LatLng[1].ToString() + $"{Convert.ToChar(176)}(E)" : (country.LatLng[1] == null ? null : country.LatLng[0].ToString() + $"{Convert.ToChar(176)}(W)")))}");
                 Console.WriteLine(String.Format("\t\t- gęstość zaludnienia: {0:F2} os/km^2",(country.Population/country.Area)));
                 Console.WriteLine($"\t\t- alpha3code: {country.Alpha3Code}");
             }
